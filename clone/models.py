1e7ffa7687d6ed18c781128b9de8e47c1d8b3041
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-16 21:11:52 +0300

    Add image model

--- clone/models.py
+++ clone/models.py
@@ -3,3 +3,11 @@ from django.db import models
 class Profile(modesls.Model):
     profile_photo =models.ImageField(upload_to ='profile/')
     bio =models.CharField()
+
+class Image(models.Model):
+    image =models.ImageField(upload_to='image/')
+    image_name=models.CharField(max_length=30)
+    image_caption=models.CharField()
+    profile=models.ForeignKey(Profile,on_delete=models.cascade)
+    likes=models.ManytoManyField(likes)
+    comments=models.TextField()
