df3767a2c7b6f4b78ec756a59371b08792852cf3
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-17 13:11:37 +0300

    Add imageform

--- clone/forms.py
+++ clone/forms.py
@@ -6,3 +6,7 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['Likes', 'pub_date', 'Profile']
