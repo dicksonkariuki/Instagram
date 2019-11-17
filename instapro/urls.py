347b23916e4a05f66af3ffd8031de685fbc949fc
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-17 10:25:36 +0300

    Add registration url patters in the project

--- instapro/urls.py
+++ instapro/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns=[
     url(r'^admin/', admin.site.urls),
-    url(r'^clone/',include('clone.urls'))
+    url(r'^clone/',include('clone.urls')),
+    url(r'^accounts/',include('registration.backend.simple.urls')),
 ]
