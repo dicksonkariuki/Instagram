ec68916a1a2a2e7d3254a071aa9e23c159e815d4
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-17 21:24:50 +0300

    Add logo style

--- clone/models.py
+++ clone/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime as dt
+from django.conf import settings
 
 class Profile(models.Model):
     profile_photo =models.ImageField(upload_to ='profile/')
--- instapro/settings.py
+++ instapro/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myproject.clone',
-    'myproject.bootstrap3',
-    'myproject.registration',
+    'clone',
+    'bootstrap3',
+    
     
 ]
 
