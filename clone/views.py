7f79f9901a0681f26c727fe47072e55f97dbb51d
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-17 13:11:04 +0300

    Add profileform

--- clone/views.py
+++ clone/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Comment,Image,Profile
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from.forms import ProfileForm,ImageForm,CommentForm
+from django.contrib.auth.decorators import login_required
 
 def welcome(request):
     return render(request,'welcome.html')
