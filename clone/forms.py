7ae2ece10f0deacb968700c21c6b0aeb3cc4ca7b
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-17 13:12:15 +0300

    Add comment form

--- clone/forms.py
+++ clone/forms.py
@@ -10,3 +10,8 @@ class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['Likes', 'pub_date', 'Profile']
+        
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name']
