8cb7ac08a11a62b01b69f3733a477d48febcec1b
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-17 09:23:58 +0300

    Add image name test

--- clone/tests.py
+++ clone/tests.py
@@ -1,4 +1,9 @@
 from django.test import TestCase
 from .models import Image,Comment,Profile
 
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.post = Image(image_name='Music',image_caption='music is a southing sound that moves souls')
 
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post,Image))
